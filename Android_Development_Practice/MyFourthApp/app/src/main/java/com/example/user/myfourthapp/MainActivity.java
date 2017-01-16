63516f25b14c6f41a75f380cc39afc440f4248bf
unknown <hamzabhatti93@gmail.com>
2017-01-16 17:58:08 +0000

    Timer works. Use handler to schedule number increment.

--- Android_Development_Practice/MyFourthApp/app/src/main/java/com/example/user/myfourthapp/MainActivity.java
+++ Android_Development_Practice/MyFourthApp/app/src/main/java/com/example/user/myfourthapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.user.myfourthapp;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Record the number of seconds passed and whether the stopwatch is running
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Start the runTimer() method when the activity is created
+        runTimer();
     }
+
+    public void onClickStart(View view) {
+        // The stopwatch will run
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        // The stopwatch will stop
+        running = false;
+    }
+
+    public void onClickReset (View view) {
+        // Reset all
+        running = false;
+        seconds = 0;
+    }
+
+    public void runTimer() {
+        // Get reference to the textView timeView
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+
+        // Define new handler, use this class to schedule cod that will be run in the future
+        final Handler handler = new Handler();
+
+        // post() accepts on paramter, object Runnable (job you want to run)
+        // Runnable objects defines a run() function
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+
+                if (running) {
+                    seconds++;
+                }
+                // postDelayed() takes Runnable and Long
+                // Runnable is the code you want to run, Long is number of milliseconds you want
+                // to delay
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
