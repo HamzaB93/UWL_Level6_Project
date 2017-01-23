c9a5dbe1e670780e781f8086e11179a8a2ba36e3
unknown <hamzabhatti93@gmail.com>
2017-01-23 14:18:01 +0000

    Add java Workout class and workoutdetailfragment topull data from class

---
+++ Android_Development_Practice/WorkoutFragment/app/src/main/java/com/example/user/workoutfragment/WorkoutDetailFragment.java
@@ -0,0 +1,48 @@
+package com.example.user.workoutfragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class WorkoutDetailFragment extends Fragment {
+
+    private long workoutId;
+
+    public WorkoutDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    // Called when the fragment is about to get shown
+    // getView gets the fragments root view
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        View view = getView();
+
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            //get reference to the Workout from the array workouts
+            Workout workout = Workout.workouts[(int) workoutId];
+            // Set the title xml TextView
+            title.setText(workout.getName());
+
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
+    }
+
+    public void setWorkout(long id) {
+        this.workoutId = id;
+    }
+}
