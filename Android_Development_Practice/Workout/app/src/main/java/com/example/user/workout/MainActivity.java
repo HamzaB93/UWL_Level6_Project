f15e4e8728bb677314c5c1e75ba291fcad2111a1
unknown <hamzabhatti93@gmail.com>
2017-01-23 19:49:24 +0000

    Error with fragment in oncreate. Trouble with types of fragment.

--- Android_Development_Practice/Workout/app/src/main/java/com/example/user/workout/MainActivity.java
+++ Android_Development_Practice/Workout/app/src/main/java/com/example/user/workout/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        
+        WorkoutDetailFragment frag = (WorkoutDetailFragment)
+                getFragmentManager().findFragmentById(R.id.detail_frag);
     }
 
     @Override
