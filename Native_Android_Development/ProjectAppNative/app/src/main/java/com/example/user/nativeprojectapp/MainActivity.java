aeb34660db3fc0cd67d2f004234dccc081dbb476
unknown <hamzabhatti93@gmail.com>
2017-02-03 16:02:34 +0000

    Add listener in mainactivity to go to a new imagetestactivity.

--- Native_Android_Development/ProjectAppNative/app/src/main/java/com/example/user/nativeprojectapp/MainActivity.java
+++ Native_Android_Development/ProjectAppNative/app/src/main/java/com/example/user/nativeprojectapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.nativeprojectapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +22,17 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         // PUT LISTENER HERE
+        AdapterView.OnItemClickListener clickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> listView, View v, int position, long id) {
+                if(position == 0) {
+                    Intent intent = new Intent (MainActivity.this, ImageTestActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        // Referencing the list view and setting the listener above
+        ListView listOptions = (ListView) findViewById(R.id.list_options);
+        listOptions.setOnItemClickListener(clickListener);
     }
 
     @Override
