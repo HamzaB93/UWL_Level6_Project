49d45b8cb2ed777fe7cf02f7e7ce3559fcf2f311
unknown <hamzabhatti93@gmail.com>
2017-02-03 19:07:20 +0000

    All images become invisible when button is pressed.

--- Native_Android_Development/ProjectAppNative/app/src/main/java/com/example/user/nativeprojectapp/ImageTestActivity.java
+++ Native_Android_Development/ProjectAppNative/app/src/main/java/com/example/user/nativeprojectapp/ImageTestActivity.java
@@ -27,11 +27,24 @@ public class ImageTestActivity extends AppCompatActivity {
         ImageView and5 = (ImageView) findViewById(R.id.android_image5);
         ImageView and6 = (ImageView) findViewById(R.id.android_image6);
 
-        if(and1.getVisibility() == View.VISIBLE) {
+        if (and1.getVisibility() == View.VISIBLE) {
             and1.setVisibility(View.INVISIBLE);
         }
-
-
+        if (and2.getVisibility() == View.VISIBLE) {
+            and2.setVisibility(View.INVISIBLE);
+        }
+        if (and3.getVisibility() == View.VISIBLE) {
+            and3.setVisibility(View.INVISIBLE);
+        }
+        if (and4.getVisibility() == View.VISIBLE) {
+            and4.setVisibility(View.INVISIBLE);
+        }
+        if (and5.getVisibility() == View.VISIBLE) {
+            and5.setVisibility(View.INVISIBLE);
+        }
+        if (and6.getVisibility() == View.VISIBLE) {
+            and6.setVisibility(View.INVISIBLE);
+        }
     }
 
 }
